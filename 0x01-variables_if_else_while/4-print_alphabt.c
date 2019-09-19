2a2ccb533dcd495102d9d3c3909d038d69a05e33
Noah172 <jsebastian.garcia1995@gmail.com>
2019-09-19 20:58:57 +0000

    alfabeto sin la 'e' o la 'q'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	if (alp == 'e')
+		{
+		alp = 'e';
+		} 
+	else if (alp == 'q')
+		{
+		alp = 'q';
+		}
+	else
+		{
+		putchar (alp);
+		}
+		putchar ('\n');
+
+	return (0);
+}
