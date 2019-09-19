7f8c16f14c89d2a59ff501680d0aeaff411fa1a2
Noah172 <jsebastian.garcia1995@gmail.com>
2019-09-19 18:00:15 +0000

    positive and negative numbers with condicionals

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,13 @@ int main(void)
 	{
 	printf("%i is positive\n", n);
 	}
-	else
-	{
 	if (n < 0)
 	{
 		printf("%i  is negative \n", n);
 	}
-	else
+	if (n == 0)
 	{
 			printf("%i is zero", n);
-	}}
+	}
 	return (0);
 }
