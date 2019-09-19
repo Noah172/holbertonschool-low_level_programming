16da526f4bcc347f18aa8c2f588bd13ec5cd13f4
Noah172 <jsebastian.garcia1995@gmail.com>
2019-09-19 21:34:34 +0000

    numeros del 0 al 9 con 2 putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = 48 ; num <= 57 ; num++)
+		putchar (num);
+		putchar ('\n');
+	return (0);
+}
