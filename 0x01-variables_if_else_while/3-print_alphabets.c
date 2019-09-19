1abc25343ddd08ab0d2bca725ccdc5737316c9c8
Noah172 <jsebastian.garcia1995@gmail.com>
2019-09-19 19:42:30 +0000

    printing alphabet in lowercaps and then in uppercaps and a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	{
+		putchar (alp);
+	}
+	for (alp = 'A' ; alp <= 'Z' ; alp++)
+		putchar (alp);
+		putchar ('\n');
+	return (0);
+}
