358a6b8bce98cff2b0bf89643f1975dcb41b02d6
Noah172 <jsebastian.garcia1995@gmail.com>
2019-09-24 13:56:06 +0000

    return 1 is alphabet and 0 if not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 
-/*
-*here i will try again the print
-*
-*
-*
-*/
-
+/**
+ * _isalpha - Print if is lower or upper case
+ *
+ * @c: is the representation of char in ascii code
+ * Return: 1 lowercase and lowercase and 0 if other case
+ */
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
