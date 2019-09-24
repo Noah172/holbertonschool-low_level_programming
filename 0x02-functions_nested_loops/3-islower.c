5ae6c1153ffb6b1b106c6dbd728f515f6fad0385
Noah172 <jsebastian.garcia1995@gmail.com>
2019-09-24 13:46:06 +0000

    return 1 if the character is lowercase and 0 other way

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
-/*
-*
-*
-*
-*
-*/
+/**
+ * _islower - Print if is lower or upper case
+ *
+ * @c is the representation of char in ascii code
+ * Return: 1 lowercase and 0 to lowercase
+ */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122 )
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
