07a4ef2d05eea3d79ac67f64adf804e999843b16
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-03 20:53:41 +0000

    print array in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *reverse_array - function that invert the string
+ *@a: is the string
+ *@n: the number of elements of the array
+ *
+ */
+void reverse_array( int *a, int n)
+{
+	int i, j;
+
+	i = n - 1;
+	j = 0;
+	while (i > j)
+	{
+		int tp = a[i];
+
+		a[i] = a[j];
+		a[j] = tp;
+		i--;
+		j++;
+	}
+}
