89be47c630c5209ff66bf01c15729c1e9a62c515
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-05 21:03:29 +0000

    capitalize the first leter of every word in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
+ *cap_string - capitalizes all words of the string
+ *@s: is the variable of the string
  *
- *
- *
- *
+ *Return: the string capitalized.
  */
 
 char *cap_string(char *s)
@@ -13,11 +13,23 @@ char *cap_string(char *s)
 
 	for (a = 0 ; s[a] != '\0' ; a++)
 	{
-		if(s[a] >= 'a' && s[a] <= 'z')
+		if (s[a] >= 'a' && s[a] <= 'z')
 		{
-			if (s[a - 1] == ' ' || s[a - 1] == '	' || s[a- 1] == '\n' || s[a - 1] == ',' || s[a - 1] == ';' || s[a - 1] == '.' || s[a - 1] == '?' || s[a - 1] == '"' || s[a - 1] == '(' || s[a - 1] == ')' || s[a - 1] == '{' || s[a - 1] == '}')
+			if (s[a - 1] == ' ' || s[a - 1] == '	' || s[a - 1] == '\n')
 			{
-			s[a] = s[a] - 32;
+				s[a] = s[a] - 32;
+			}
+			else if (s[a - 1] == ',' || s[a - 1] == ';' || s[a - 1] == '.')
+			{
+				s[a] = s[a] - 32;
+			}
+			else if (s[a - 1] == '(' || s[a - 1] == ')' || s[a - 1] == '{')
+			{
+				s[a] = s[a] - 32;
+			}
+			else if (s[a - 1] == '"' || s[a - 1] == '}' || s[a - 1] == '?')
+			{
+				s[a] = s[a] - 32;
 			}
 		}
 	}
