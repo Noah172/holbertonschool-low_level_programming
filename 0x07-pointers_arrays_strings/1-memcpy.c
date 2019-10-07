8b25f1f7fe05fa32a4c1cd3ea831de0723d7eab7
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-07 18:52:13 +0000

    copie memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c <= n ; c++)
+	{
+		dest[c] = src[c];
+	}
+
+	return (dest);
+}
