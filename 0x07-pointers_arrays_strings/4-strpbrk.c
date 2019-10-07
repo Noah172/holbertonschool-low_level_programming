1958882acf5714d27ec84fcdd4165015728af225
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-07 20:59:48 +0000

    find for a match in two strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
+ *_strpbrk - find matches in two strings
+ *@s: the first string and the destiny to print
+ *@accept: the comparation string
+ *Return: always to 0.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int c, d;
 
-	for(c = 0; s[c] != '\0'; c++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		for(d = 0; accept[d] != '\0'; d++)
+		for (d = 0; accept[d] != '\0'; d++)
 		{
-			if (s[c] == accept [d])
+			if (s[c] == accept[d])
 			{
 				return (s + c);
 			}
@@ -26,5 +26,4 @@ char *_strpbrk(char *s, char *accept)
 		return (NULL);
 
 	return (0);
-		
 }
