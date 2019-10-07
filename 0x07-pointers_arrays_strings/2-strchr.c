4365f4803c1a1eb0c1ab18b091d5cbb8ecbc9cf0
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-07 20:03:39 +0000

    find a char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,11 +18,11 @@ char *_strchr(char *s, char c)
 		{
 			return (s + i);
 		}
-		else if(s[i] = '\0')
-		{
-			return (NULL);
-		}
 		i++;
+	}
+	if (s[i] == '\0')
+	{
+		return (NULL);
 	}
 
 	return (0);
