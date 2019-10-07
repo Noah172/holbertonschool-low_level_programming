0a5ee9c3dcf5b9766058d439069c57230f0eb9a7
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-07 19:03:31 +0000

    copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int c;
+	unsigned int d = 0;
 
 	for (c = 0; c <= n ; c++)
 	{
-		dest[c] = src[c];
+		dest[c] = src[d];
 	}
 
 	return (dest);
