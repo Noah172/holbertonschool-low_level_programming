0fe2d8446f2894f6e93b67569acaeb6c49152adf
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-10 16:46:54 +0000

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,10 +2,10 @@ int prime(int n, int x);
 #include "holberton.h"
 
 /**
+ *is_prime_number - is the funtion that return the 0
+ *@n: is the int var
  *
- *
- *
- *
+ *Return: 0 if is not prime and 1 if it is 
  */
 
 int is_prime_number(int n)
@@ -19,10 +19,10 @@ int is_prime_number(int n)
 }
 
 /**
- *
- *
- *
- *
+ *prime - make the operation to see if is a prime number
+ *@n: the number we want to know if is prime
+ *@x: the divisor to see if n is prime
+ *Return: 0 or 1
  */
 
 int prime(int n, int x)
