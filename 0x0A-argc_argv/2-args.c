1c7295647cd42e0ebf4a714c982a04a4450047fd
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-12 18:03:39 +0000

    print the args followed by a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+}
