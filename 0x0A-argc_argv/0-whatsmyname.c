072e36ed59e7261eb1b9c8eac7f3afd5139413d1
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-12 18:15:46 +0000

    prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ *main - where the arg will be taken
+ *@c: a int variable
+ *@argv: the array of the args
+ *Return: always to 0.
  */
 
-int main(int c, char* argv[])
+int main(int c, char *argv[])
 {
 	c = 0;
 
 	printf("%s\n", argv[c]);
-	
-	return(0);
+
+	return (0);
 }
