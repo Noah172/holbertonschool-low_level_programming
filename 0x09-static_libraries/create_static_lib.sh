c76c3b2a715bea0da6c9a93868a1fcd5d616e822
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-13 01:35:51 +0000

    script that create libraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-cat list|cut -d "." -f1|
-while read CD_KND
-do
-Filename=$(find ../ -name "${CD_KND}.c" | head -n1)
-echo "creating ${CD_KND} file"
-gcc -g -O -c "${Filename}" -o "${CD_KND}.o"
-ar rc libholberton.a "${CD_KND}.o"
-done
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a
