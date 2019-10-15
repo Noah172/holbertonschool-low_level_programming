26ca2176e2ac8c6041a4f5f3a0fadd7ec653eb5a
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-15 15:43:45 +0000

    create a new string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - function that returns a pointer to a newly allocated space in memory.
+ *@str: pointer to the string
+ *
+ *Return: the new pointer
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *neo;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		}
+
+		neo = malloc(i * sizeof(char));
+
+		for (j = 0; j <= i; j++)
+		{
+			neo[j] = str[j];
+		}
+		return (neo);
+	}
+	return (0);
+}
