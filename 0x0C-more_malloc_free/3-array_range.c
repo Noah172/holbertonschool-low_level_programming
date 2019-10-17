9c6d971ebaccb8592a19d436055a14735d515ec0
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-17 21:22:05 +0000

    create a int array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int a, b;
+	int *ar;
+
+	if (min > max)
+		return (NULL);
+
+	b = max - min;
+
+	ar = malloc((b + 1) * sizeof(int));
+	if (ar == NULL)
+		return (NULL);
+
+	for (a = 0; a <= b; a++)
+	{
+		ar[a] = min + a;
+	}
+	
+	return (ar);
+}
