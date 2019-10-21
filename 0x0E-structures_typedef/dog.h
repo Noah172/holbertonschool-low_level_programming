792844932dffe4cdca4659b79897d1b92ced1d32
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-21 21:12:16 +0000

    a puppy!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOGHEADER
+#define DOGHEADER
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - take data of a doggo
+ * @name: name of the doggo
+ * @age: how old is the doggo
+ * @owner: who keeps alive the doggo
+ *
+ * Description: take 3 data from a dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
