c367aaa4dc7eb327dae8439897dbea6e8cf430fa
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-23 18:57:17 +0000

    arrays pointes and functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- *
- *
+ *array_iterator - make iterations with arrays in functions
+ *@array: the array given
+ *@size: the size of the array
+ *@action: action to make in the array
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
