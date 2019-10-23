9f661f93b1fcd7a3beb25707413a059949698af5
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-23 18:06:48 +0000

    print the names calling it as a pointer to the functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *print_name - calls the other two funtions
+ *@f: a void pointer
+ *@name: the name of the person
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
