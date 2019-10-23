167ba6fe3a99d2567988100dc9c7555af949873a
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-23 03:58:44 +0000

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,5 @@
 #ifndef DOGHEADER
 #define DOGHEADER
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * struct dog - take data of a doggo
  * @name: name of the doggo
@@ -10,15 +8,14 @@
  *
  * Description: take 3 data from a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
-/** prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* HOLBERTON_H */
