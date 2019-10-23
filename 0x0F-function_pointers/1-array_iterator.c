32cef63b76cef636c89b1775fdf71a5a335541c6
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-23 18:53:58 +0000

    arrays pointes and functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,5 +7,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	
+	unsigned int a;
+
+	if (array != NULL && size && action != NULL)
+		for (a = 0; a < size; a++)
+		{
+			action(array[a]);
+		}
 }
