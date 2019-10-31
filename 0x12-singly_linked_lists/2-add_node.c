59fb01d49cbcd99c8ca91403fdbb2430ef7b8868
Noah172 <jsebastian.garcia1995@gmail.com>
2019-10-31 21:30:27 +0000

    add modules to a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ *add_node - add new nodes to the list
+ *@head: the head of the node
+ *@str: the string of the node
+ *Return: 
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int len = 0;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+
+	if (str != NULL)
+	{
+		for (len = 0; str && str[len]; len++)
+			;
+		new->str = strdup(str);
+	}
+	else
+	{
+		new->str = NULL;
+	}
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
