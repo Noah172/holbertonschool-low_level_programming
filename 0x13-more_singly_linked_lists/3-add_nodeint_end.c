b3147ac01705c1698b6601389a3c01aed42a01c1
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-05 15:58:15 +0000

    fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *add_nodeint_end
+ *add_nodeint_end - add nodes in the end of the list
  *@head: the head of the list
  *@n: constant int
  */
@@ -20,8 +20,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		last = *head;
+
 		while (last->next != NULL)
+		{
 				last = last->next;
+		}
 		last->next = new;
 	}
 	return (*head);
