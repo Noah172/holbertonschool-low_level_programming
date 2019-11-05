eb562dcf4b007c36679612fb9be494344a1c6667
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-05 16:24:20 +0000

    add node at the end of the list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,6 +3,7 @@
  *add_nodeint_end - add nodes in the end of the list
  *@head: the head of the list
  *@n: constant int
+ *Return: the new head
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
@@ -17,15 +18,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head = new;
-	else
-	{
-		last = *head;
 
-		while (last->next != NULL)
-		{
-				last = last->next;
-		}
-		last->next = new;
+	last = *head;
+
+	while (last->next != NULL)
+	{
+		last = last->next;
 	}
+	last->next = new;
+
 	return (*head);
 }
