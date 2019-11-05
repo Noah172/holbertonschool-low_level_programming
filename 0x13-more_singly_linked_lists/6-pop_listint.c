4df44138dacf066e1aceb323d26f732f8d78a3a1
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-05 20:52:23 +0000

    pop out

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ *pop_listint - pops the first item of the list
+ *@head: head of the list
+ *Return: 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int r, c = 0;
+	listint_t *tmp, *del;
+
+	if (head == NULL)
+		return (0);
+
+	del = *head;
+
+	r = del->n;
+
+	tmp = *head;
+
+
+	while(c < 1)
+	{
+		tmp = tmp->next;
+		c++;
+	}
+
+	*head = tmp;
+
+	return (r);
+}
