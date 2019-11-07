5ab0115dd66e7aad5037b1c6b9030c11717c3a75
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-07 20:37:09 +0000

    bit solicited

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *get_bit - give the bit solicited of a number
+ *@n: the number 
+ *@index: the bit solicited
+ *Return: the bit or -1 if fail
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
