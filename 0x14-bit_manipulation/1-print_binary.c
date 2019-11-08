7ff39835c3025be5c1ea586a898f7cd19f0c7048
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-08 15:59:40 +0000

    convert to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,12 +6,16 @@
 void print_binary(unsigned long int n)
 {
 	int bit = sizeof(n) * 8, ban = 0;
-	while  (bit)
+
+	while (bit)
 	{
-		if (n & 1L << --bit) {
+		if (n & 1L << --bit)
+		{
 			putchar(1 + '0');
 			ban++;
-		} else if (ban){
+		}
+		else if (ban)
+		{
 			putchar(0 + '0');
 		}
 	}
