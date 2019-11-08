ad13297ce412e5824af59aa9e410b87e6a1d26ce
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-08 04:19:18 +0000

    add node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ *
+ *
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int c = 0;
+	listint_t *back, *front, *newn = malloc(sizeof(listint_t));
+
+	if (newn == NULL)
+		return (NULL);
+
+	newn->n = n;
+	back = *head;
+	front = *head;
+
+	if (idx == 0)
+	{
+		newn->next = *head;
+		*head = newn;
+		return (newn);
+	}
+
+	front = front->next;
+
+	while (c < idx - 1)
+	{
+		back = back->next;
+		front = front->next;
+		c++;
+		if (back == NULL)
+			return (NULL);
+	}
+	if (c == idx)
+	{
+		free (newn);
+		return (NULL);
+	}
+
+	back->next = newn;
+	newn->next = front;
+
+	return (newn);
+}
