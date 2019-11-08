092361fe81493c8fda624f46929911e637810989
Noah172 <jsebastian.garcia1995@gmail.com>
2019-11-08 15:51:40 +0000

    convert to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8, ban = 0;
+	while  (bit)
+	{
+		if (n & 1L << --bit) {
+			putchar(1 + '0');
+			ban++;
+		} else if (ban){
+			putchar(0 + '0');
+		}
+	}
+		if (!ban)
+			putchar(0 + '0');
+}
