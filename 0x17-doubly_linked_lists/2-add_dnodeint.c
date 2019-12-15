6cb09ae0127c4c07729d2a4bcafbb9e4adbcf475
Noah172 <jsebastian.garcia1995@gmail.com>
2019-12-15 19:33:49 +0000

    add a node in the head of the list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,11 +10,24 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *neo;
 
 	neo = malloc(sizeof(dlistint_t));
-	if (neo == NULL || n == '\0')
+	if (neo == NULL)
 		return (NULL);
-	neo->n = n;
-	neo->prev = NULL;
-	neo->next = *head;
-	*head = neo;
+
+	if (n == '\0')
+		return (NULL);
+
+	if (head != NULL)
+	{
+		neo->n = n;
+		neo->prev = NULL;
+		neo->next = *head;
+	}
+	else
+	{
+		neo->n = n;
+		neo->next = NULL;
+		neo->prev = NULL;
+		*head = neo;
+	}
 	return (neo);
 }
