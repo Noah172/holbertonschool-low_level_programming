bca416aace5008b85fc93644117ac2c561708639
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-19 22:56:36 +0000

    printing

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ *hash_table_print - function that prints a hash table.
+ *@ht: the hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *printer = NULL;
+	unsigned long int c = 0;
+	char *cm = "";
+
+	if (ht)
+	{
+		printer = ht->array[c];
+		putchar('{');
+		while (c < ht->size)
+		{
+			while (printer)
+			{
+				printf("%s'%s': '%s'", cm, printer->key, printer->value);
+				cm = ", ";
+				printer = printer->next;
+			}
+		}
+		putchar('}');
+	}
+}
