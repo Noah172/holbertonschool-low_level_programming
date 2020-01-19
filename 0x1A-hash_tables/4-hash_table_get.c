70dab993e60624820c11e6bfe97d795cf8ad5117
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-19 21:52:08 +0000

    seeking

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,7 @@
  *@ht: the hash table you want to look into.
  *@key: the key you are looking for.
  *
- *Return: the value associated with the element, or NULL if key couldnt be found.
+ *Return: the value associated with the element, NULL if key couldnt be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
