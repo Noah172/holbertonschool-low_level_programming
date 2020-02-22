2c54a003bbb30924782988a6b9ef9cf5402f4ca5
Noah172 <jsebastian.garcia1995@gmail.com>
2020-02-22 16:56:44 +0000

    greed island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
     p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                p += 1
-
-    p += 1
-    pf = p * 2
-    return pf
+    nr = len(grid)
+    nc = len(grid[0])
+    y = -1
+    for maps in grid:
+        y += 1
+        x = -1
+        for land in maps:
+            x += 1
+            if land == 1:
+                if x == 0 or grid[y][x - 1] != 1:
+                    p += 1
+                if x == nc - 1 or grid[y][x + 1] != 1:
+                    p += 1
+                if y == 0 or grid[y - 1][x] != 1:
+                    p += 1
+                if y == nr - 1 or grid[y + 1][x] != 1:
+                    p += 1
+    return p
