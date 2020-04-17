e6cc8fd576925d514988fbbb5a9500cb27169ee1
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-16 22:34:24 -0500

    spaces for tabs

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,14 +12,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-        int i;
-        if (!array)
-                return (-1);
-        for (i = 0; i < (int)size; i++)
-        {
-                printf("Value checked array[%d] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
-        }
-        return (-1);
+	int i;
+	if (!array)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
